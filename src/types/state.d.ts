f4a65afd003a1a400337f97e3891349acb189964
minkyung00 <mksin00@naver.com>
2023-01-10 19:32:07 +0900

    feat: state와 setState 타입 설정하는 StateAndAction 타입 추가

---
+++ src/types/state.d.ts
@@ -0,0 +1,7 @@
+import { Dispatch, SetStateAction } from 'react';
+
+type StateAndAction<T, K extends string> = {
+  [P in K]: T;
+} & {
+  [P in `set${Capitalize<K>}`]: Dispatch<SetStateAction<T>>;
+};
