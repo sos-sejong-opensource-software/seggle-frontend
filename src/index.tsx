dd04a4f18c975827c7c01675f33013a434055b3c
minkyung00 <mksin00@naver.com>
2023-01-10 19:18:06 +0900

    chore: axios, react-query 설치 및 초기 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
 import './style/index.css';
 
 const root = document.getElementById('root') as HTMLElement;
+const queryClient = new QueryClient();
 
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
