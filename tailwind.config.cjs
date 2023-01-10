1987e85aec74672cb9abc205444833323a5d5131
minkyung00 <mksin00@naver.com>
2023-01-10 21:31:07 +0900

    chore: Pretendard 폰트 설치 및 tailwind 커스텀 설정
    - font-size 커스텀
    - color 커스텀

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,23 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: ['./src/**/*.{html,js,ts,tsx,jsx}'],
   theme: {
+    fontSize: {
+      sm: '0.75rem',
+      base: '1rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '1.875rem',
+      '3xl': '2.25rem',
+      '4xl': '3rem',
+    },
+    fontFamily: {
+      Pretendard: 'Pretendard',
+    },
     extend: {
       colors: {
         primary: colors.indigo,
+        error: colors.red,
+        disabled: colors.slate,
       },
     },
   },
