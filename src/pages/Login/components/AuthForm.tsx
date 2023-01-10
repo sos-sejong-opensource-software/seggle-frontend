48ab2fa76620159fd1c529c1f733df8f878ea3be
minkyung00 <mksin00@naver.com>
2023-01-10 21:33:52 +0900

    refactor: Atom 컴포넌트로 변경
    - pull 받기 전에 임시로 구현한 컴포넌트를 Atom 컴포넌트로 교체

--- src/pages/Login/components/AuthForm.tsx
+++ src/pages/Login/components/AuthForm.tsx
@@ -1,30 +1,8 @@
+import { Label, Input, Button, ErrorMessage } from '@/components';
 import { PAGE } from '@/constants';
 
 import { LOGIN_ERROR, REGISTER_ERROR } from '../constants';
 import { useInput } from '../hooks';
-
-/** FIXME: Atom 컴포넌트로 대체될 부분 */
-type AtomProps<T extends React.ElementType> = Component<T>;
-
-function Label({ children, ...props }: AtomProps<'label'>) {
-  return <label {...props}>{children}</label>;
-}
-
-function Input({ ...props }: AtomProps<'input'>) {
-  return <input type="text" {...props} />;
-}
-
-function ErrorMessage({ children, ...props }: AtomProps<'p'>) {
-  return <p {...props}>{children}</p>;
-}
-
-function Button({ children, ...props }: AtomProps<'button'>) {
-  return (
-    <button type="submit" {...props}>
-      {children}
-    </button>
-  );
-}
 
 type AuthFormProps = {
   mode: 'login' | 'register';
@@ -38,11 +16,12 @@ export function AuthForm({ mode, inputList, onSubmit }: AuthFormProps) {
   const isFormValid = inputList.every((input) => input.valid());
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className="flex flex-col gap-3">
       {inputList.map(({ id, label, value, setValue, error, setError, valid }, index) => (
         <>
           <Label htmlFor={id}>{label}</Label>
           <Input
+            type={id.includes('password') ? 'password' : undefined}
             id={id}
             value={value}
             onChange={({ target: { value } }) => {
