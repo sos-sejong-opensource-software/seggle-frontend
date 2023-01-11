eae7da02e41accd4a2dc0eb4afd964f4d8e4fa1f
minkyung00 <mksin00@naver.com>
2023-01-11 21:36:56 +0900

    feat: 회원가입 API 구현 (#7)
    - 기본 API_URL 수정
    
    Related to: #7

--- src/pages/User/api/index.ts
+++ src/pages/User/api/index.ts
@@ -2,10 +2,14 @@ import { AxiosResponse } from 'axios';
 
 import { api } from '@/api';
 
-const API_URL = `/users/login/`;
+const API_URL = `/users/`;
 
 const loginUser = (payload: User): Promise<AxiosResponse<LoginResponse>> => {
+  return api.post(`${API_URL}/login`, payload);
+};
+
+const registerUser = (payload: RegisterRequest): Promise<AxiosResponse> => {
   return api.post(`${API_URL}`, payload);
 };
 
-export { loginUser };
+export { loginUser, registerUser };
