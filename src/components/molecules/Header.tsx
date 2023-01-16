ea540ce5bf145b4880a7dcb6802cf2a079e4c61b
judahhh <jdh99080516@gmail.com>
2023-01-17 00:31:50 +0900

    fix: merge 충돌 수정

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Header = () => {
+// import { tw } from '@/utils/tailwindMerge';
+
+export function Header() {
   return (
     <>
       <header className="fixed inset-x-0 top-0 z-50 left-0 bg-white text-gray-700 body-font border-b border-gray-200">
@@ -31,6 +33,6 @@ const Header = () => {
       </header>
     </>
   );
-};
+}
 
 export default Header;
