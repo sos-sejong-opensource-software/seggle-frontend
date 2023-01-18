39ba7ea9931307bb1ef2d843fbea16a940907a6d
minkyung00 <mksin00@naver.com>
2023-01-18 15:00:31 +0900

    fix: Modal 컴포넌트 수정 (#10)
    - 재사용성을 위해 합성 컴포넌트로 수정
    - 메인 컴포넌트: ModalContainer
    - 서브 컴포넌트: Header, Main, Footer
    
    Related to: #10

--- src/components/molecule/Modal.tsx
+++ src/components/molecule/Modal.tsx
@@ -1,71 +1,66 @@
-import React from "react";
+import { createPortal } from 'react-dom';
 
-export default function Modal() {
-  const [showModal, setShowModal] = React.useState(false);
-  return (
-    <>
-      <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Open regular modal
-      </button>
-      {showModal ? (
+import { tw } from '@/utils/tailwindMerge';
+import { Heading } from '@/components';
+
+type ModalProps<T extends React.ElementType> = Component<T> & {
+  open: boolean;
+};
+type ModalItemProps<T extends React.ElementType> = Component<T>;
+
+function ModalContainer({ open, children }: ModalProps<'div'>) {
+  return open
+    ? createPortal(
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
-              </div>
+          <Dimmed />
+          <div className="items-center flex fixed inset-0 z-50">
+            <div className="flex flex-col mx-auto max-w-3xl p-6 rounded-lg shadow-lg bg-white">
+              {children}
             </div>
           </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
-    </>
+        </>,
+        document.body
+      )
+    : null;
+}
+
+function Header({ className, children, ...props }: ModalItemProps<'header'>) {
+  return (
+    <header className={tw('p-5 border-b border-slate-200', className)} {...props}>
+      <Heading as="h4">{children}</Heading>
+    </header>
   );
 }
+
+function Body({ className, children, ...props }: ModalItemProps<'main'>) {
+  return (
+    <main className={tw('p-6', className)} {...props}>
+      {children}
+    </main>
+  );
+}
+
+function Footer({ className, children, ...props }: ModalItemProps<'footer'>) {
+  return (
+    <footer className={tw('flex justify-end p-6 border-t border-slate-200', className)} {...props}>
+      {children}
+    </footer>
+  );
+}
+
+function Dimmed({ className, ...props }: ModalItemProps<'div'>) {
+  return (
+    <div
+      className={tw('fixed inset-0 z-40 bg-black opacity-40', className)}
+      aria-hidden="true"
+      {...props}
+    ></div>
+  );
+}
+
+export const Modal = Object.assign(ModalContainer, {
+  Dimmed,
+  Header,
+  Body,
+  Footer,
+});
