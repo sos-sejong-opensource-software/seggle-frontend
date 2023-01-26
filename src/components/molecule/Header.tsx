a22714551810e9db49bfca6ab4b156d8c98a1eb2
minkyung00 <mksin00@naver.com>
2023-01-26 16:05:06 +0900

    refactor: Header 컴포넌트 수정
    - 재사용성을 위해 합성 컴포넌트로 수정
    - 메인 컴포넌트: HeaderContainer
    - 서브 컴포넌트: Menu, MenuList, Logo, ToggleButton,

--- src/components/molecule/Header.tsx
+++ src/components/molecule/Header.tsx
@@ -1,38 +1,87 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-// import { tw } from '@/utils/tailwindMerge';
+import { NavLink, LinkProps } from 'react-router-dom';
 
-export function Header() {
+import { tw } from '@/utils/tailwindMerge';
+
+type HeaderProps<T extends React.ElementType> = Component<T>;
+type MenuListProps<T extends React.ElementType> = Component<T> & {
+  menuList: { name: string; to: string }[];
+};
+type ToggleButtonProps<T extends React.ElementType> = Component<T> & {
+  on: boolean;
+};
+
+function HeaderContainer({ className, children, ...props }: HeaderProps<'header'>) {
   return (
-    <>
-      <header className="fixed inset-x-0 top-0 z-50 left-0 bg-white text-gray-700 body-font border-b border-gray-200">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3  text-2xl text-black-500">Seggle</span>
-          </div>
-          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <div className="hidden md:flex header">
-              <Link className="mr-5 hover:text-gray-900 hover:font-bold" to="/contest">
-                대회
-              </Link>
-              <Link className="mr-5 hover:text-gray-900 hover:font-bold" to="class">
-                수업
-              </Link>
-              <Link className="mr-5 hover:text-gray-900 hover:font-bold" to="board">
-                건의게시판
-              </Link>
-              <Link className="mr-5 hover:text-gray-900 hover:font-bold" to="announcements">
-                공지사항
-              </Link>
-              <Link className="mr-5 hover:text-gray-900 hover:font-bold" to="faq">
-                FAQ
-              </Link>
-            </div>
-          </nav>
-        </div>
-      </header>
-    </>
+    <header className={tw('', className)} {...props}>
+      {children}
+    </header>
   );
 }
 
-export default Header;
+function Logo({ className, children, ...props }: HeaderProps<'span'>) {
+  return (
+    <span className={tw('ml-3 text-lg font-bold sm:text-xl', className)} {...props}>
+      <NavLink to="/">{children}</NavLink>
+    </span>
+  );
+}
+
+function Menu({ to, className, children, ...props }: LinkProps) {
+  return (
+    <li>
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          tw(`hover:font-bold ${isActive ? 'font-bold' : ''}`, className)
+        }
+        {...props}
+      >
+        {children}
+      </NavLink>
+    </li>
+  );
+}
+
+function MenuList({ menuList, className, children, ...props }: MenuListProps<'ul'>) {
+  return (
+    <ul className={tw('', className)} {...props}>
+      {menuList.map(({ name, to }, index) => (
+        <Menu to={to} key={index}>
+          {name}
+        </Menu>
+      ))}
+      {children}
+    </ul>
+  );
+}
+
+function ToggleButton({ on, onClick, className, ...props }: ToggleButtonProps<'button'>) {
+  return (
+    <button type="button" onClick={onClick} className={tw('w-6 h-6', className)} {...props}>
+      <span className="sr-only">메인 메뉴 열기</span>
+      <div aria-hidden="true">
+        <span
+          className={tw(
+            `block h-[1px] w-5 bg-current transform transition duration-300 ease-in-out ${
+              on ? '-rotate-45 translate-y-[1px]' : 'translate-y-1.5'
+            }`
+          )}
+        ></span>
+        <span
+          className={tw(
+            `block h-[1px] w-5 bg-current transform transition duration-300 ease-in-out ${
+              on ? 'rotate-45 translate-y-0' : '-translate-y-1.5'
+            }`
+          )}
+        ></span>
+      </div>
+    </button>
+  );
+}
+
+export const Header = Object.assign(HeaderContainer, {
+  Menu,
+  MenuList,
+  Logo,
+  ToggleButton,
+});
