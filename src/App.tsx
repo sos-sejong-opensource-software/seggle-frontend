1c07d3ef6c83557f8a03f50809dc0fae8ac237bb
inyeong-kang <tomatorkd@naver.com>
2023-01-28 19:19:55 +0900

    feat: 건의사항 페이지 제작 및 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 
 import { Login, Register, Faq } from '@/pages';
 import { Announcement } from "./pages/Announcement/Announcement";
+import { Board } from "./pages/Board/Board";
+
 import { MainHeader } from '@/components';
 import { PATH } from '@/constants';
 
@@ -17,8 +19,8 @@ export default function App() {
         <Route path={PATH.REGISTER} element={<Register />} />
         <Route path={PATH.COMPETITION_LIST} element={<div>CompetitionList</div>} />
         <Route path={PATH.CLASS_LIST} element={<div>ClassList</div>} />
-        <Route path={PATH.BOARD_LIST} element={<div>BoardList</div>} />
         <Route path={PATH.ANNOUNCEMENT_LIST} element={<Announcement />} />
+        <Route path={PATH.BOARD_LIST} element={<Board />} />
         <Route path={PATH.FAQ} element={<Faq />} />
 
         <Route path="*" element={<div>Not Found</div>} />
---
+++ src/pages/Board/Board.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+import { Heading, Table } from '@/components';
+
+import { useProposal } from './hooks'
+
+export function Board() {
+
+  const ProposalTableData = useProposal();
+  console.log(ProposalTableData);
+  const [currentPage, setCurrentPage] = useState(1);
+  const lastPage = 20;
+  
+  const data = {
+        "count": 3,
+        "next": null,
+        "previous": null,
+        "results": [
+            {
+                "id": 6,
+                "title": "112323",
+                "created_user": "seggle",
+                "created_time": "2023-01-27T20:33:05.578832"
+            },
+            {
+                "id": 5,
+                "title": "112323",
+                "created_user": "seggle",
+                "created_time": "2023-01-27T20:31:28.631619"
+            },
+            {
+                "id": 4,
+                "title": "qwer",
+                "created_user": "seggle",
+                "created_time": "2023-01-27T14:56:03.176323"
+            }
+        ]
+    }
+  const column = [
+    { Header: '#', accessor: 'id' },
+    { Header: '제목', accessor: 'title' },
+    { Header: '작성자', accessor: 'created_user' },
+    { Header: '작성일', accessor: 'created_time' },
+  ];
+
+  const handleRowClick = (
+    e: React.MouseEvent<HTMLTableRowElement, MouseEvent>,
+    id: number | string
+  ) => {
+    console.log(id);
+  };
+
+  return (
+    <>
+      <Heading>건의게시판</Heading>
+      <Table column={column} data={data.results} onRowClick={handleRowClick} />
+    </>
+  );
+}
+//<h1 className="text-3xl text-blue-400 px-3 py-3">Table</h1>
+//<Table column={column} data={data} onRowClick={handleRowClick} />
