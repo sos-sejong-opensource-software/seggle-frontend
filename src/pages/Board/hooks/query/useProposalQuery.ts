ea95019104d2ec9453bd18d9c1445970cc4b5e51
inyeong-kang <tomatorkd@naver.com>
2023-01-28 19:09:08 +0900

    feat: 건의사항 리스트 조회 커스텀 훅 구현

---
+++ src/pages/Board/hooks/query/useProposalQuery.ts
@@ -0,0 +1,21 @@
+import { useQuery, UseQueryOptions } from 'react-query';
+import { AxiosError } from 'axios';
+
+import { QUERY_KEYS } from '@/constants';
+
+import { getProposal } from '../../api';
+
+export const useAnnouncementQuery = (
+  options?: UseQueryOptions<ProposalResponse, AxiosError, ProposalResponse, string>
+) => {
+  return useQuery(
+    QUERY_KEYS.PROPOSAL,
+    async () => {
+      const { data } = await getProposal();
+      return data;
+    },
+    {
+      ...options,
+    }
+  );
+};
