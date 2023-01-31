6ace36d841462ab45b34d30606e1da680c3b5859
inyeong-kang <tomatorkd@naver.com>
2023-01-31 16:49:58 +0900

    fix: 건의사항 리스트 조회 API 관련 기존 query를 SuspenseQuery 방식으로 수정

--- src/pages/Board/hooks/query/useProposalQuery.ts
+++ src/pages/Board/hooks/query/useProposalQuery.ts
@@ -1,6 +1,7 @@
 import { useQuery, UseQueryOptions } from 'react-query';
 import { AxiosError } from 'axios';
 
+import { useSuspenseQuery } from '@/hooks/useSuspenseQuery';
 import { QUERY_KEYS } from '@/constants';
 
 import { getProposal } from '../../api';
@@ -8,8 +9,8 @@ import { getProposal } from '../../api';
 export const useProposalQuery = (
   options?: UseQueryOptions<ProposalResponse, AxiosError, ProposalResponse, string>
 ) => {
-  return useQuery(
-    QUERY_KEYS.PROPOSAL,
+  return useSuspenseQuery(
+    QUERY_KEYS.FAQ,
     async () => {
       const { data } = await getProposal();
       return data;
