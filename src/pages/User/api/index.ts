faa912b2e32a167f145a49206a15f40c06b8392a
joo-prog <1114nhj@naver.com>
2023-02-06 14:37:54 +0900

    feat: 검색 useSearch로 변경

--- src/pages/User/api/index.ts
+++ src/pages/User/api/index.ts
@@ -5,7 +5,7 @@ import { api } from '@/api';
 const API_URL = `/users/`;
 
 const loginUser = (payload: User): Promise<AxiosResponse<LoginResponse>> => {
-  return api.post(`${API_URL}/login`, payload);
+  return api.post(`${API_URL}/login/`, payload);
 };
 
 const registerUser = (payload: RegisterRequest): Promise<AxiosResponse> => {
