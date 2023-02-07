4fe602a7614b1341c0057b0d8dd0eb2ebefb1c9c
joo-prog <1114nhj@naver.com>
2023-02-07 18:28:45 +0900

    feat: switch 내부에서 상태 값 가지도록 변경
    * button의 onClick으로 작동하다보니 변경된 값을 가져올 수 없음
    * 그래서 switch 내부에서 value를 갖도록 함

--- src/components/atom/Switch.tsx
+++ src/components/atom/Switch.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type SwitchProps<T extends React.ElementType> = Component<T> & {
   enabled: boolean;
+  onSwitchClick?: (value: boolean) => void;
 };
 
-export function Switch({ enabled, onClick, className }: SwitchProps<'button'>) {
+export function Switch({ enabled, onSwitchClick, className }: SwitchProps<'button'>) {
+  const [value, setValue] = useState(enabled);
+
   return (
     <div className={className}>
       <label className="inline-flex relative items-center cursor-pointer">
-        <input type="checkbox" className="sr-only peer" checked={enabled} readOnly />
+        <input type="checkbox" className="sr-only peer" checked={value} readOnly />
         <button
-          onClick={onClick}
+          onClick={() => {
+            setValue(!value);
+            onSwitchClick && onSwitchClick(!value);
+          }}
           className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary-300"
         ></button>
       </label>
