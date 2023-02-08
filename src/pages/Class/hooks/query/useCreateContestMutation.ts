2c2b30291a55002aaef6a5ad6b9df73fe7b590cd
minkyung00 <mksin00@naver.com>
2023-02-09 00:04:41 +0900

    fix: contest 생성 후 목록 다시 불러오기 (#50)

--- src/pages/Class/hooks/query/useCreateContestMutation.ts
+++ src/pages/Class/hooks/query/useCreateContestMutation.ts
@@ -1,5 +1,7 @@
-import { useMutation, UseMutationOptions } from 'react-query';
 import { AxiosError, AxiosResponse } from 'axios';
+import { useMutation, UseMutationOptions, useQueryClient } from 'react-query';
+
+import { QUERY_KEYS } from '@/constants/queryKeys';
 
 import { createContest } from '../../api';
 
@@ -8,7 +10,11 @@ type useCreateContestProps = { classId: string; payload: ContestRequest };
 export const useCreateContestMutation = (
   options?: UseMutationOptions<AxiosResponse, AxiosError, useCreateContestProps>
 ) => {
+  const queryClient = useQueryClient();
   return useMutation(({ classId, payload }) => createContest({ classId, payload }), {
     ...options,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries(QUERY_KEYS.CLASS_CONTEST);
+    },
   });
 };
