cba92e60e364efd0bd1f039581655536f7bedf93
inyeong-kang <tomatorkd@naver.com>
2023-02-10 17:38:59 +0900

    style(component): Article Template tailwind css 적용

--- src/components/template/Article.tsx
+++ src/components/template/Article.tsx
@@ -13,47 +13,63 @@ type ArticleProps<T extends React.ElementType> = Component<T> & {
 
 export function Article({ detail }: ArticleProps<'article'>) {
   return (
-    <div className="flex flex-col">
-      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="overflow-hidden">
-            <table className="min-w-full border text-center">
+    <div className={tw('flex flex-col')}>
+      <div className={tw('overflow-x-auto sm:-mx-6 lg:-mx-8')}>
+        <div className={tw('py-2 inline-block min-w-full sm:px-6 lg:px-8')}>
+          <div className={tw('overflow-hidden')}>
+            <table className={tw('min-w-full border text-center')}>
               <tbody>
-                <tr className="border-b">
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">
-                    <Heading as="h5" className="font-semibold">
+                <tr className={tw('border-b')}>
+                  <td
+                    className={tw(
+                      'px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r'
+                    )}
+                  >
+                    <Heading as="h5" className={tw('font-semibold')}>
                       제목
                     </Heading>
                   </td>
                   <td
-                    className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r"
+                    className={tw(
+                      'px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r'
+                    )}
                     colSpan={3}
                   >
                     {detail.title}
                   </td>
                 </tr>
-                <tr className="border-b">
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">
-                    <Heading as="h5" className="font-semibold">
+                <tr className={tw('border-b')}>
+                  <td
+                    className={tw(
+                      'px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r'
+                    )}
+                  >
+                    <Heading as="h5" className={tw('font-semibold')}>
                       작성자
                     </Heading>
                   </td>
                   <td>{detail.created_user}</td>
                   <td>
-                    <Heading as="h5" className="font-semibold">
+                    <Heading as="h5" className={tw('font-semibold')}>
                       작성일
                     </Heading>
                   </td>
                   <td>{detail.created_time}</td>
                 </tr>
-                <tr className="border-b">
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">
-                    <Heading as="h5" className="font-semibold">
+                <tr className={tw('border-b')}>
+                  <td
+                    className={tw(
+                      'px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r'
+                    )}
+                  >
+                    <Heading as="h5" className={tw('font-semibold')}>
                       내용
                     </Heading>
                   </td>
                   <td
-                    className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r"
+                    className={tw(
+                      'px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r'
+                    )}
                     colSpan={3}
                   >
                     <pre>{detail.context}</pre>
