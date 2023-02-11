384b2d2769585676a5223bb5086ee67e8adf4daf
inyeong-kang <tomatorkd@naver.com>
2023-02-11 20:41:43 +0900

    feat(Board): 건의게시판 세부사항 조회 및 건의사항 생성, 수정, 삭제 관련 API 구현

--- src/pages/Board/api/index.ts
+++ src/pages/Board/api/index.ts
@@ -2,10 +2,26 @@ import { AxiosResponse } from 'axios';
 
 import { api } from '@/api';
 
-const API_URL='/proposals'
+const API_URL = `/proposals`;
 
 const getProposal = (): Promise<AxiosResponse<ProposalResponse>> => {
   return api.get(`${API_URL}`);
 };
 
-export { getProposal };
+const getProposalDetail = (proposalId: string): Promise<AxiosResponse<ProposalDetailResponse>> => {
+  return api.get(`${API_URL}/${proposalId}/`);
+};
+
+const createProposal = (payload: CreateEditProposalRequest) => {
+  return api.post(`${API_URL}/`, payload);
+};
+
+const editProposal = (proposalId: string, payload: CreateEditProposalRequest) => {
+  return api.put(`${API_URL}/${proposalId}/`, payload);
+};
+
+const deleteProposal = (proposalId: string): Promise<AxiosResponse<DeleteProposalResponse>> => {
+  return api.delete(`${API_URL}/${proposalId}`);
+};
+
+export { getProposalDetail, getProposal, createProposal, editProposal, deleteProposal };
--- src/types/proposal.d.ts
+++ src/types/proposal.d.ts
@@ -6,8 +6,26 @@ type ProposalResult = {
 }[];
 
 type ProposalResponse = {
-    count: number;
-    next: string;
-    previous: string;    
-    results: ProposalResult;
+  count: number;
+  next: string;
+  previous: string;
+  results: ProposalResult;
+};
+
+type ProposalDetailResponse = {
+  id: number;
+  title: string;
+  context: string;
+  created_time: string;
+  created_user: string;
+};
+
+type CreateEditProposalRequest = {
+  title: string;
+  context: string;
+};
+
+type DeleteProposalResponse = {
+  title: string;
+  context: string;
 };
