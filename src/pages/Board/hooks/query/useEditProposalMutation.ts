9e7e56d9fee0baa6f1c1aeea4842304fafed5172
inyeong-kang <tomatorkd@naver.com>
2023-02-12 15:27:10 +0900

    feat(Board): 건의게시판 글 수정 후 목록으로 돌아가는 기능 구현

--- src/pages/Board/hooks/query/useEditProposalMutation.ts
+++ src/pages/Board/hooks/query/useEditProposalMutation.ts
@@ -1,4 +1,8 @@
+import { PATH } from '@/constants';
+
 import { useMutation, UseMutationOptions } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+
 import { AxiosError, AxiosResponse } from 'axios';
 
 import { editProposal } from '../../api';
@@ -7,10 +11,12 @@ export const useEditProposalMutation = (
   proposalId: string,
   options?: UseMutationOptions<AxiosResponse, AxiosError, FormData>
 ) => {
+  const navigate = useNavigate();
   return useMutation((payload) => editProposal(proposalId, payload), {
     ...options,
     onSuccess: () => {
-      alert('편집 완료');
+      alert('수정 완료');
+      navigate(`${PATH.BOARD}`);
     },
   });
 };
