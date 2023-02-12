4965c7031fc680b1f26d411dec6543e4e3f31bd5
inyeong-kang <tomatorkd@naver.com>
2023-02-12 15:04:52 +0900

    feat(Borad): 건의게시판 삭제 API를 이용하여 구현

--- src/pages/Board/BoardDetail.tsx
+++ src/pages/Board/BoardDetail.tsx
@@ -2,14 +2,25 @@ import { useParams } from 'react-router-dom';
 
 import { Article } from '../../components/Article';
 import { Button } from '@/components';
-import { useProposalDetailQuery } from './hooks/query/useProposalDetailQuery';
+import { useProposalDetailQuery, useDeleteProposalMutation } from './hooks/query';
 
 export function BoardDetail() {
-  const proposalId = useParams().id;
-  const { data } = useProposalDetailQuery(String(proposalId));
+  const { mutate: deleteProposal } = useDeleteProposalMutation();
+
+  const proposalId = String(useParams().id);
+  const { data } = useProposalDetailQuery(proposalId);
 
   const goBoardList = () => {
     history.back();
+  };
+
+  const handleDeleteButtonClick = ({ e }: { e: React.MouseEvent<HTMLElement> }) => {
+    e.stopPropagation();
+    const isConfirmed = confirm(`건의사항을 삭제하시겠습니까?`);
+    if (isConfirmed) {
+      deleteProposal(proposalId);
+      goBoardList();
+    }
   };
 
   return (
@@ -17,7 +28,7 @@ export function BoardDetail() {
       <Button onClick={goBoardList}>목록</Button>
       <Article detail={data}></Article>
       <Button>수정</Button>
-      <Button>삭제</Button>
+      <Button onClick={(e) => handleDeleteButtonClick({ e })}>삭제</Button>
     </>
   );
 }
--- src/pages/Board/hooks/query/index.ts
+++ src/pages/Board/hooks/query/index.ts
@@ -1,2 +1,4 @@
 export * from './useProposalQuery';
 export * from './useProposalDetailQuery';
+export * from './useDeleteProposalMutation';
+export * from './useEditProposalMutation';
