d8a1bab40620614e23dd267b3fd1d3c0bd41f6f5
inyeong-kang <tomatorkd@naver.com>
2023-02-12 15:24:35 +0900

    feat(Board/components): 건의게시판 글 생성 및 수정 폼 구현

---
+++ src/pages/Board/components/ProposalCreateEditForm.tsx
@@ -0,0 +1,46 @@
+import { Button, Input, Label } from '@/components';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type ProposalCreateEditFormProps = {
+  data?: {
+    title: string;
+    context: string;
+  };
+  onProposalMutate?: (data: CreateEditProposalRequest) => void;
+};
+
+export function ProposalCreateEditForm({ data, onProposalMutate }: ProposalCreateEditFormProps) {
+  const navigate = useNavigate();
+  const [title, _context] = data ? [data.title, data.context] : ['', ''];
+  const [context, setContext] = useState(_context);
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    const data = {
+      title: String(formData.get('title')) ?? '',
+      context,
+    };
+    if (onProposalMutate !== undefined) onProposalMutate(data);
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit} className="w-full">
+      <>
+        <Label>제목</Label>
+        <Input className="w-full" defaultValue={title} name="title" />
+      </>
+      <>
+        <Label>내용</Label>
+      </>
+      <div className="flex float-right gap-2">
+        <Button type="button" onClick={() => navigate(-1)}>
+          취소
+        </Button>
+        <Button>저장</Button>
+      </div>
+    </form>
+  );
+}
