9ee271310b521c5bf695e81e09c7a4611151af14
inyeong-kang <tomatorkd@naver.com>
2023-02-13 12:27:58 +0900

    feat(Announcement): SuspenseQuery를 이용하여 공지사항 전체 리스트 조회 구현 완료

--- src/types/announcement.d.ts
+++ src/types/announcement.d.ts
@@ -1,25 +1,33 @@
-type AnnouncementResponse = {
-  count: number;
-  next?: number | null;
-  previous?: number | null;
-  results?: AnnouncementResult | [];
-};
-
 type AnnouncementResult = {
   id: number;
   title: string;
   created_time: string;
   last_modified: string;
-  visible: boolean;
   important: boolean;
+  visible: boolean;
 }[];
 
 type AnnouncementDetail = {
   id: number;
   title: string;
-  created_user: string;
   created_time: string;
+  created_user: string;
   last_modified: string;
-  visible: boolean;
   important: boolean;
+  visible: boolean;
+};
+
+type AnnouncementResponse = {
+  count: number;
+  next: string;
+  previous: string;
+  results: AnnouncementResult;
+};
+
+type AnnouncementDetailResponse = {
+  id: number;
+  title: string;
+  context: string;
+  created_time: string;
+  created_user: string;
 };
