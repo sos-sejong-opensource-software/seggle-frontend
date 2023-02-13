beeade60aa2bbbb29d0ec575000c0a9a331e9dc4
inyeong-kang <tomatorkd@naver.com>
2023-02-13 12:08:49 +0900

    feat(Announcement): 공지사항 세부사항 조회 쿼리 커스텀 훅 구현

--- src/types/announcement.d.ts
+++ src/types/announcement.d.ts
@@ -1,25 +1,25 @@
 type AnnouncementResponse = {
-    count: number;
-    next?: number | null;
-    previous?: number | null;    
-    results?: AnnouncementResult | [];
-  };
-  
+  count: number;
+  next?: number | null;
+  previous?: number | null;
+  results?: AnnouncementResult | [];
+};
+
 type AnnouncementResult = {
-    id: number;
-    title: string;
-    created_time: string;
-    last_modified: string;
-    visible: boolean;
-    important: boolean;
+  id: number;
+  title: string;
+  created_time: string;
+  last_modified: string;
+  visible: boolean;
+  important: boolean;
 }[];
-  /**
-   *     
-   * 
-   id: number;
-    title: string;
-    created_time: string;
-    last_modified: string;
-    visible: boolean;
-    important: boolean;
-   */
+
+type AnnouncementDetail = {
+  id: number;
+  title: string;
+  created_user: string;
+  created_time: string;
+  last_modified: string;
+  visible: boolean;
+  important: boolean;
+};
