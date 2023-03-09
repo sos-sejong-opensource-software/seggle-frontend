360da9001caf450d4bd2514e6053d1c9ec3f8e56
minkyung00 <mksin00@naver.com>
2023-03-09 22:53:59 +0900

    fix: 전체 문제 편집 api 삭제

--- src/pages/Class/AllProblems/hooks/query/index.ts
+++ src/pages/Class/AllProblems/hooks/query/index.ts
@@ -1,5 +1,4 @@
 export * from './useAllProblemListQuery';
 export * from './useProblemQuery';
-export * from './useEditProblemMutation';
 export * from './useEditProblemPublicMutation';
 export * from './useDeleteProblemMutation';
